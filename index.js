da4e6bad8fa0a68dc988b492952483980d6c706d
Mike790 <michael.kydd@outlook.com>
2024-04-17 14:11:24 -0400

    finish project

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
+const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const dns = require('dns');
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
@@ -14,10 +17,28 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+let urlDatabase = [];
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.post('/api/shorturl/', (req, res) => {
+  if (!req.body.url.includes('http')) {
+    res.json({ error: 'invalid url'})
+    return
+  }
+  
+  urlPair = {
+    original_url: req.body.url,
+    short_url: urlDatabase.length 
+  }
+
+  urlDatabase.push(urlPair)
+
+  res.json(urlPair)
+})
+
+app.get('/api/shorturl/:shortUrlId', (req, res) => {
+  let urlPair = urlDatabase.find(urlPair => urlPair.short_url === Number(req.params.shortUrlId))
+  res.redirect(urlPair.original_url)
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
